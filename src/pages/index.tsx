d126e1671fc236c134af6617849a8abdb1cc034a
KarthikS373 <lit2021012@iiitl.ac.in>
2023-02-23 19:30:26 +0530

    hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import React from "react"
 
+import Hero from "../components/home/hero/Hero"
+
 const HomePage = () => {
-  return <div className="center h-screen w-screen">TAIM</div>
+  return (
+    <div className="w-screen">
+      <Hero />
+    </div>
+  )
 }
 
 export default HomePage
