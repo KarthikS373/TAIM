1a40f45a6016bc292f330cc34c6001cd59d22a02
KarthikS373 <lit2021012@iiitl.ac.in>
2023-03-02 03:32:35 +0530

    map

--- src/pages/city/[city].tsx
+++ src/pages/city/[city].tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react"
+// @ts-ignore
+import { csv } from "d3-request"
 import { useRouter } from "next/router"
 import { Map } from "react-map-gl"
+import { Color } from "react-three-fiber"
 
 import { HexagonLayer } from "@deck.gl/aggregation-layers/typed"
 import { AmbientLight, LightingEffect, PointLight } from "@deck.gl/core/typed"
@@ -29,7 +32,7 @@ const pointLight2 = new PointLight({
 
 const lightingEffect = new LightingEffect({ ambientLight, pointLight1, pointLight2 })
 
-const material = {
+const material: any = {
   ambient: 0.64,
   diffuse: 0.6,
   shininess: 32,
@@ -48,7 +51,7 @@ const INITIAL_VIEW_STATE = {
 
 const MAP_STYLE = "https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json"
 
-export const colorRange = [
+export const colorRange: any = [
   [1, 152, 189],
   [73, 227, 206],
   [216, 254, 181],
@@ -72,7 +75,6 @@ function getTooltip({ object }: any) {
 }
 
 const City = ({
-  data,
   mapStyle = MAP_STYLE,
   radius = 1000,
   upperPercentile = 100,
@@ -80,33 +82,46 @@ const City = ({
 }: any) => {
   const router = useRouter()
 
+  const [hexData, setHexData] = useState<any>(null)
+
   const { city } = router.query
 
-  const layers = [
-    new HexagonLayer({
-      id: "heatmap",
-      //   colorRange,
-      coverage,
-      data,
-      elevationRange: [0, 3000],
-      elevationScale: data && data.length ? 50 : 0,
-      extruded: true,
-      getPosition: (d: any) => d,
-      pickable: true,
-      radius,
-      upperPercentile,
-      //   material,
+  useEffect(() => {
+    csv(DATA_URL, (error: any, response: any) => {
+      if (!error) {
+        const data = response.map((d: any) => [Number(d.lng), Number(d.lat)])
+        const layers = [
+          new HexagonLayer({
+            id: "heatmap",
+            colorRange,
+            coverage,
+            data,
+            elevationRange: [0, 3000],
+            elevationScale: data && data.length ? 50 : 0,
+            extruded: true,
+            getPosition: (d: any) => d,
+            pickable: true,
+            radius,
+            upperPercentile,
+            material,
 
-      transitions: {
-        elevationScale: 3000,
-      },
-    }),
-  ]
+            transitions: {
+              elevationScale: 3000,
+            },
+          }),
+        ]
+
+        setHexData(layers)
+      } else {
+        console.log(error)
+      }
+    })
+  }, [])
 
   return (
     <div className="h-screen">
       <DeckGL
-        layers={layers}
+        layers={hexData}
         effects={[lightingEffect]}
         initialViewState={INITIAL_VIEW_STATE}
         controller={true}
